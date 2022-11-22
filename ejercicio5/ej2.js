aeb9b3efa0b4ece2888dd5597614c93cec079cee
Dennis <dennis.061998@gmail.com>
2022-11-21 21:29:32 -0300

    Ej nuevo

---
+++ ejercicio5/ej2.js
@@ -0,0 +1,14 @@
+
+function max(contenido) {
+    var mx=0;
+for (var i=0; i<contenido.length; i++) {
+    for (var j=1; i<contenido.length; j++) {
+    if(contenido[i]>contenido[j]) {
+        mx=contenido[i]
+    }
+
+}
+}
+return mx;
+}
+console.log(max([1,3,2]));
---
+++ ejercicio5/sum.js
@@ -0,0 +1,12 @@
+
+function sum(contenido) {
+    var suma=0;
+
+for (var i=0; i<contenido.length; i++) {
+suma = suma+contenido[i];
+
+
+}
+return suma;
+}
+console.log(sum([1,2,3]));
